281e8980659875da1ba93ea39271c6bb465a4c42
Sarthak Doshi <sarthakdoshi2310@gmail.com>
2025-07-30 10:28:38 +0530

    fixes

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from pydantic import BaseModel, HttpUrl, Field
 # Document processing imports
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 
